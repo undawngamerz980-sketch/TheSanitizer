71f689cb8b644d859e860c59947ae8d7e63557e2
undawngamerz980-sketch <undawngamerz980@gmail.com>
2026-05-09 05:05:12 -0700

    Adding files for Project.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ConsoleProgramSanitizer
+{
+    class Program
+    {
+            public static void Main(string[] args)
+            {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            int smth = 0;
+            Console.WriteLine("Program started.");
+            int[] result = ProgramDependency.randominitializer(10);
+            int[] sanitizedarray = Programdeptwo.Sanitizer(result);
+
+            Console.Write("<| Printing for integrity reveal:");
+            foreach (int i in sanitizedarray)
+            {
+                Console.Write($"/Index{smth}Value{sanitizedarray[i]}");
+                smth++;
+            }
+            Console.Write("IntegrityRevealDone |>");
+        }
+    }
+}
