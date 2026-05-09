a5545554d361bcc521df910e35b98bf6cf92b153
undawngamerz980-sketch <undawngamerz980@gmail.com>
2026-05-09 05:36:39 -0700

    added better usability support.

---
+++ ConsoleProgramSanitizer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleProgramSanitizer
+{
+    class Program
+    {
+            public static void Main(string[] args)
+            {
+            Console.WriteLine("Program started.");
+            int[] result = ProgramDependency.randominitializer(10);
+            int[] sanitizedarray = Programdeptwo.Sanitizer(result);
+
+            Console.Write("<| Printing for integrity reveal:");
+            for (int i = 0; i < sanitizedarray.Length; i++)
+            {
+                Console.Write($"/Index{i}Value{sanitizedarray[i]}");
+            }
+            Console.Write("IntegrityRevealDone |>");
+        }
+    }
+}
